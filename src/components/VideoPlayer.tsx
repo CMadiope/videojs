eadf9eeacdb604655dd7069945bb769492601f36
Confidence Madiope <confidencem97@gmail.com>
2024-07-16 12:02:27 +0200

    .

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -4,7 +4,7 @@ import { useRef, useEffect, useState } from "react";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
 
-const VideoPlayer = (props) => {
+const VideoPlayer = (props:any) => {
     const videoRef = useRef<HTMLDivElement>(null);
     const playerRef = useRef<any>(null);
     const [playbackRate, setPlaybackRate] = useState(1);
